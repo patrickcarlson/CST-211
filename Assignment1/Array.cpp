b79de49bda8b2ea03db1c0ee037c427c76c3b0b1
Patrick Carlson <patrick.carlson@oit.edu>
2015-09-29 18:37:37 -0700

    Assignment1 setup
    Setting up first assignment, familiarizing with GIT

---
+++ Assignment1/Array.cpp
@@ -0,0 +1,11 @@
+#include "Array.h"
+
+
+Array::Array()
+{
+}
+
+
+Array::~Array()
+{
+}
---
+++ Assignment1/Array.h
@@ -0,0 +1,10 @@
+#pragma once
+
+template <typename T>
+class Array
+{
+public:
+	Array();
+	~Array();
+};
+
