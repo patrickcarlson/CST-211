a07e75b60f7e1185c2cb10512d9b534617bcd3b6
Patrick Carlson <patrick.carlson@oit.edu>
2015-09-30 12:08:14 -0700

    Setup Assignment 1
    Setting up first assignment. Starting constructors.

--- Assignment1/Array.cpp
+++ Assignment1/Array.cpp
@@ -1,11 +1,61 @@
 #include "Array.h"
 
-
-Array::Array()
+template <typename T>
+Array<T>::Array()
 {
+
 }
 
-
-Array::~Array()
+template <typename T>
+Array<T>::Array(int length, int start_index = 0)
+	:m_length(length), m_start_index(start_index)
 {
+	m_array = new T[length];
+}
+
+template <typename T>
+Array<T>::Array(const Array& copy)
+{
+	
+}
+
+template <typename T>
+Array<T>::~Array()
+{
+
+}
+
+template <typename T>
+Array& Array<T>::operator =(const Array& rhs)
+{
+
+}
+
+template <typename T>
+T& Array<T>::operator[](int index)
+{
+
+}
+
+template <typename T>
+int Array<T>::getStartIndex()
+{
+
+}
+
+template <typename T>
+void Array<T>::setStartIndex(int start_index)
+{
+
+}
+
+template <typename T>
+int Array<T>::getLength()
+{
+
+}
+
+void Array<T>::setLength(int length)
+{
+
 }
--- Assignment1/Array.h
+++ Assignment1/Array.h
@@ -1,10 +1,25 @@
-#pragma once
+#ifndef ARRAY_H
+#define ARRAY_H
 
 template <typename T>
 class Array
 {
 public:
 	Array();
+	Array(int length, int start_index = 0);
+	Array(const Array& copy);
 	~Array();
+	Array& operator =(const Array& rhs);
+	T& operator[](int index);
+	int getStartIndex();
+	void setStartIndex(int start_index);
+	int getLength();
+	void setLength(int length);
+
+private:
+	T* m_array;
+	int m_length;
+	int m_start_index;
 };
 
+#endif
