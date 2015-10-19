55aa33367774098f136424671ed2bc275500dd40
Patrick Carlson <patrick.carlson@oit.edu>
2015-10-19 09:24:28 -0700

    Lab 1 Initial
    Initial commit for lab one

---
+++ Lab1/Array2D.h
@@ -0,0 +1,108 @@
+/***********************************************************
+* Author:					Patrick Carlson
+* Date Created:				10/8/2015
+* Last Modification Date:	10/11/2015
+* Assignment Number:		CST 211 Assignment 2
+* Filename:					Array2D.h
+*
+* Overview:
+*	Array2D class is a Multidimensional array. The size of the
+*	array can be shortened or lengthened.
+*
+* Input:
+*	Input for the class created is applied in Tester.cpp to
+*	test the uses of the class. Outside input includes row length
+*	and column length.
+*
+* Output:
+*	This class does not provide output implicitly, but their
+*	stored values can be reached through member functions.
+************************************************************/
+
+#ifndef ARRAY2D_H
+#define ARRAY2D_H
+
+#include "Row.h"
+#include "Exception.h"
+
+/************************************************************************
+* Class: Array2D
+*
+* Constructors:
+*		Array2D()
+*				m_row and m_col are set to 0, a 1 x 1 array is created.
+*		Array2D(int row, int col = 0)
+*				m_row set to row value and m_col set to col value, otherwise
+*				m_col set to 0(default). Creates multidimensional array using
+*				T** m_array member variable.
+*		Array(const Array& copy)
+*				New Array2D object member variables are set to same values
+*				as copied Array2D object.
+*
+* Mutators:
+*		void setRow(int rows)
+*				This mutator allows resizing of the 2d array's rows. Must be 
+*				set greater than 0.
+*		void setColumn(int columns)
+*				This mutator allows resizing of the 2d array's columns. Must 
+*				be set greater than 0.
+*
+* Methods:
+*		Array2D& operator=(const Array2D &rhs);
+*				Assignment operator overload
+*		Row<T> operator[](int index);
+*				Subscript operator overload.
+*		int getRow();
+*				Accessor for m_row member variable. Returns number of rows.
+*		int getColumn();
+*				Accessor for m_col member variable. Returns number of columns
+*		T& Select(int row, int column);
+*				Return reference to item in prescribed row and column.
+*************************************************************************/
+
+template <typename T>
+class Array2D
+{
+public:
+	// Default Constructor
+	Array2D();
+	
+	// Two argument constructor
+	Array2D(int row, int col = 0);
+	
+	// Copy constructor
+	Array2D(const Array2D &copy);
+	
+	// Descructor
+	~Array2D();
+
+	// Assignment operator overload
+	Array2D& operator=(const Array2D &rhs);
+	
+	// Subscript operator overload
+	Row<T> operator[](int index);
+
+	// m_row member variable accessor.
+	int getRow();
+
+	// m_row mutator and array resize
+	void setRow(int rows);
+
+	// m_col member variable accessor
+	int getColumn();
+
+	// m_col mutator and array resize
+	void setColumn(int columns);
+
+	// Cell mutator
+	T& Select(int row, int column);
+
+private:
+	T** m_array;		// Pointer to start of array of pointers.
+	int m_row;			// Number of rows in array
+	int m_col;			// Number of columns in array
+};
+
+#include "Array2D.inc"
+
+#endif
---
+++ Lab1/Exception.h
@@ -0,0 +1,92 @@
+/***********************************************************
+* Author:				Patrick Carlson
+* Date Created:			10/1/2015
+* Last Modification Date:	10/17/2015
+* Assignment Number:    CST 211 Assignment 3
+* Filename:				Exception.h
+*
+* Overview:
+*	This files contains the Exception class, used to throw
+*	custom error messages for invalid parameters set by
+*	users.
+*
+* Input:
+*	Constructor takes an input of a String which is used
+*	as the message when an exception is thrown.
+*
+* Output:
+*	Utilizes the overloaded ostream operator to easily
+*	use output methods to display thrown exception's 
+*	message.
+************************************************************/
+#ifndef EXCEPTION_H
+#define EXCEPTION_H
+
+#include <iostream>
+
+
+/************************************************************************
+* Class: Exception
+*
+* Constructors:
+*		Exception()
+*				m_msg set the string "Standard Error"
+*		Exception(char *msg)
+*				m_msg is set to argument value msg.
+*		Exception(const Exception &copy)
+*				New Exception object member variables are set to same values
+*				as copied Exception object.
+*
+* Mutators:
+*		void setMessage(char *msg)
+*				Allows mutation of the m_msg member variable.
+*
+* Methods:
+*		const char* getMessage()
+*				Inspector which returns value in member variable m_msg.
+*		friend std::ostream& operator<<(std::ostream &stream, const Exception &except)
+*				ostream operator overload.
+*		Exception& operator=(const Exception &rhs)
+*				Overloads the assignment operator for the Exception class
+*************************************************************************/
+class Exception
+{
+public:
+	// Default Constructor
+	Exception() { m_msg = "Standard Error"; }
+
+	// Single argument constructor
+	Exception(char *msg) : m_msg(msg) {}
+
+	// Copy constructor
+	Exception(const Exception &copy) { m_msg = copy.m_msg; }
+
+	// Destructor
+	~Exception(){}
+
+	// Assignment operator overload
+	Exception& operator =(const Exception &rhs) {
+		m_msg = rhs.m_msg;
+		return *this;
+	}
+
+	// m_msg member variable inspector
+	const char* getMessage() { return m_msg; }
+
+	// m_msg member variable mutator
+	void setMessage(char *msg){ m_msg = msg; }
+
+	// ostream operator overload.
+	friend std::ostream& operator<<(std::ostream &stream, const Exception &except)
+	{
+		stream << except.m_msg;
+		return stream;
+	}
+
+private:
+	char* m_msg; //Stored message for thrown exception.
+};
+
+#include "Exception.inc"
+
+#endif
