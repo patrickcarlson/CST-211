240ff8d954e52ed988e0a90131052ec8170d984a
Patrick Carlson <patrick.carlson@oit.edu>
2015-11-01 19:44:42 -0800

    Tree time
    Branching out to trees, need seperate bubble sorts.

--- CarlsonLab2/Sorts.cpp
+++ CarlsonLab2/Sorts.cpp
@@ -1,26 +1,83 @@
+#include <iostream>
+
+using namespace std;
 
 template <typename T>
-void Bubblesort(T data[]);
+void myBubbleSort(T theArray[], int n);
 
-void main()
+template <typename T>
+void myInsertionSort(T theArray[], int n);
+
+template <typename T>
+void mySelectionSort(T theArray[], int n);
+
+
+
+int main()
 {
-	int thisArray[5];
-	thisArray[0] = 11;
-	thisArray[1] = 7;
-	thisArray[2] = 3;
-	thisArray[3] = 2;
-	thisArray[4] = 25;
+	int thisArray[5] = { 11, 7, 3, 2, 25 };
+	int toSortArray[5];
 
-	Bubblesort(thisArray);
+	memcpy(toSortArray, thisArray, 5*sizeof(int));
+
+	myBubbleSort(toSortArray, 5);
+
+	memcpy(toSortArray, thisArray, 5 * sizeof(int));
+
+	myInsertionSort(toSortArray, 5);
+
+	memcpy(toSortArray, thisArray, 5 * sizeof(int));
+
+	mySelectionSort(toSortArray, 5);
+
 }
 
+
 template <typename T> 
-void BubbleSort(T data[])
+void myBubbleSort(T theArray[], int n)
 {
-	int n = data.length();
+	cout << "this is a test" << endl;
 
 	for (int i = 0; i < n - 1; i++)
 		for (int j = n - 1; j > i; --j)
-			if (data[j] < data[j - 1])
-				swap(data[j], data[j - 1]);
+			if (theArray[j] < theArray[j - 1])
+				swap(theArray[j], theArray[j - 1]);
+
+}
+
+template <typename T>
+void myInsertionSort(T theArray[], int n)
+{
+	T temp;
+	for (int i = 1, j; i < n; i++)
+	{
+		temp = theArray[i];
+		for (j = i; j > 0 && temp < theArray[j - 1]; j--)
+		{
+			theArray[j] = theArray[j - 1];
+		}
+
+		theArray[j] = temp;
+	}
+		
+
+}
+
+template <typename T>
+void mySelectionSort(T theArray[], int n)
+{
+	for (int i = 0, j, temp; i < n - 2; i++)
+	{
+		temp = i;
+
+		for (j = i + 1; j < n - 1; j++)
+		{
+			if(theArray[j] < theArray[temp])
+			{
+				temp = j;
+			}
+		}
+		if(temp != i)
+		swap(theArray[i], theArray[temp]);
+	}
 }
